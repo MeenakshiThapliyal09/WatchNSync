02752dae79ef8b868213ccc2eddce78713c2ed9a
Meenakshi <meenakshithapliyal09@gmail.com>
2026-09-13 02:15:19 +0530

    fix: wire room manager into server

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,17 @@
 import { createServer } from 'node:http'
-import { app } from './app.js'
+import { app, configureRoomRoutes } from './app.js'
+import { RoomManager } from './rooms/roomManager.js'
 import { configureSocketServer } from './socket.js'
 
 const fallbackPort = 3000
 const configuredPort = Number.parseInt(process.env.PORT ?? `${fallbackPort}`, 10)
 const port = Number.isFinite(configuredPort) ? configuredPort : fallbackPort
 const host = process.env.HOST ?? '0.0.0.0'
+const roomManager = new RoomManager()
 
+configureRoomRoutes(roomManager)
 const httpServer = createServer(app)
-configureSocketServer(httpServer)
+configureSocketServer(httpServer, roomManager)
 
 httpServer.listen(port, host, () => {
   console.info(`WatchNSync server listening on http://${host}:${port}`)
